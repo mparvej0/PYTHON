8b59c9d57bd2675dd7dc9b9686ff6f71aa9a57b7
MD PARVEJ <mparvejind@gmail.com>
2025-01-07 18:33:45 +0530

    variable

---
+++ Variables Data Types Input Output/variable_prac.py
@@ -0,0 +1,50 @@
+user_name = "Mr. Raju"
+user_age = "22"
+print(f"Hello, my name is {user_name} and I am {user_age} years old.")
+
+temperature = 25
+temperature = 20
+temperature = 35
+print(temperature, temperature, temperature)
+
+radius = 7
+c = 2 * 3.14159 * radius
+print(c)
+
+height_meters = 1.80
+weight_in_kg = 75
+bmi = weight_in_kg / (height_meters ** 2)
+print(bmi)
+
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+print("a =", a)
+print("b =", b)
+
+pi = 3.14159
+raidus = 12
+area = pi * radius
+print(area)
+
+sentence = "Python is fun!"
+print(len(sentence))
+print(sentence.upper())
+
+name = input("Enter your name ")
+age = input("Enter your age ")
+print(f"Hello {name} You are {age} years odl.")
+
+length = 5
+width = 10
+height = 15
+volume = length * width * height
+print(volume)
+
+has_licens = True
+if has_licens:
+    print("You can drive")
+else:
+    print("You can't drve")
