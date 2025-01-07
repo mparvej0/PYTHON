d51e1f03828997d77e5ce3084e0cd58e5dc49d3d
MD PARVEJ <mparvejind@gmail.com>
2025-01-07 13:18:08 +0530

    intro in python

---
+++ Introduction to Python/intro.py
@@ -0,0 +1,13 @@
+#This program prints introductory messages
+print("Hello World!")
+print("Hello, World")
+
+#This checks if one number is greater than another
+if 5 > 2:
+    print("Five is greater than two.",)
+print(4 + 3)
+
+# This is a single-lne comment.
+""""
+This is a multi-line comment.
+"""
